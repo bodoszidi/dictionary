660d7d5ccca81b3a5e5cdb43d1eade821d777ce4
bodoszidi <szidibee@gmail.com>
2025-12-30 14:22:09 +0000

    installed play audio

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,11 +1,18 @@
+import ReactAudioPlayer from "react-audio-player";
+
 export default function Phonetics(props) {
-    return (
-        <div className="Phonetics d-flex flex-column align-items-start text-start">
-            <a href={props.phonetic.audio} target="_blank"  rel="noopener noreferrer">
-                Listen
-            </a>
-            <br />
-            {props.phonetic.text}
-        </div>
-    );
+  return (
+    <div className="Phonetics d-flex flex-column align-items-start text-start">
+      <a href={props.phonetic.audio} target="_blank" rel="noopener noreferrer">
+        Listen
+      </a>
+
+      <div>{props.phonetic.text}</div>
+
+      <ReactAudioPlayer
+        src={props.phonetic.audio}
+        controls
+      />
+    </div>
+  );
 }
